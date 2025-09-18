971c292dbe308549a325840f86a68082f1e41c8c
Arthur <arthurgren4@gmail.com>
2025-09-18 14:55:50 -0300

    teste3

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Ola");
         System.out.println("Teste");
+        System.out.println("Final");
     }
 }
